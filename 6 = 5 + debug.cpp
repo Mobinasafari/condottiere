d16d5894a5d0e40419a16cd5fab0abb6755ae25b
Mobinasafari <mobinasafari2020@gmail.com>
2024-06-17 22:04:54 +0330

    Add files via upload

---
+++ 6 = 5 + debug.cpp
@@ -0,0 +1,484 @@
+
+
+#include<iostream>
+#include<ctime>
+using namespace std ;
+
+class card{
+	
+	public :
+		card( string input_name , int input_number_of_card , int input_value)
+		{
+			set_name(  input_name )	;
+			set_number_of_card( input_number_of_card );
+			set_value(input_value);
+		}
+			
+		void set_name( string input_name )
+		{
+			name = input_name ;
+		}
+		void set_number_of_card( int input_number_of_card)
+		{
+			number_of_card = input_number_of_card ;
+		}
+		void set_number_of_toozi_card( int input_number_of_toozi_card)
+		{
+			number_of_toozi_card = input_number_of_toozi_card ;
+		}
+		void set_value( int input_value )
+		{
+			value = input_value ;
+		}
+		string get_name()
+		{
+			return name ;
+		}
+		int get_number_of_card()
+		{
+			return number_of_card;
+		}
+		int get_number_of_toozi_card()
+		{
+			return number_of_toozi_card;
+		}
+		int get_value()
+		{
+			return value ;
+		}
+		virtual void print()
+		{
+			cout << "A" ;
+		}
+		
+	protected :
+		int value ;
+		string name ;
+		int number_of_card ;
+		int number_of_toozi_card = 0 ;
+		
+};
+
+class yello_card : public card{
+	
+	public :
+		yello_card( string input_name , int input_number_of_card , int input_value) : card(input_name ,input_number_of_card , input_value)
+		{
+			
+		}
+		virtual void print()
+		{
+			cout << "yello_card" << "  ";
+		}
+	
+};
+class perpule_card : public card{
+	
+	public :
+		perpule_card( string input_name , int input_number_of_card , int input_value) : card(input_name , input_number_of_card , input_value)
+
+		{
+			
+		}
+	
+		virtual void print()
+		{
+			cout << "perpule_card" << "  ";
+		}
+	
+};
+class Matarsak : public perpule_card {
+	
+	public :
+		Matarsak( string input_name , int input_number_of_card , int input_value) : perpule_card(input_name , input_number_of_card , input_value)
+		{
+			
+		}		
+		virtual void print()
+		{
+			cout << "Matarsak" << "  ";
+		}
+		
+};
+class Tablzan : public perpule_card {
+	
+	public :
+		Tablzan( string input_name , int input_number_of_card , int input_value) : perpule_card(input_name , input_number_of_card , input_value)
+		{
+			
+		}
+		virtual void print()
+		{
+			cout << "Tablzan" << "  ";
+		}
+		
+};
+class Shahdokht : public perpule_card {
+	
+	public :
+		Shahdokht( string input_name , int input_number_of_card , int input_value) : perpule_card(input_name , input_number_of_card , input_value)
+		{
+			
+		}
+		virtual void print()
+		{
+			cout << "Shahdokht" << "  ";
+		}
+		
+};
+class Bahar : public perpule_card {
+	
+	public :
+		Bahar( string input_name , int input_number_of_card , int input_value) : perpule_card(input_name , input_number_of_card , input_value)
+		{
+			
+		}
+		virtual void print()
+		{
+			cout << "Bahar" << "  ";
+		}
+		
+};
+class Zemestan : public perpule_card {
+	
+	public :
+		Zemestan( string input_name , int input_number_of_card , int input_value) : perpule_card(input_name , input_number_of_card , input_value)
+		{
+			
+		}
+		virtual void print()
+		{
+			cout << "Zemestan" << "  ";
+		}
+		
+};
+class player{
+	
+	public :
+		void set_name( string input_name )
+		{
+			name = input_name ;
+		}
+		void set_age( int input_age )
+		{
+			age = input_age ;
+		}
+		void set_color( string input_color)
+		{
+			color = input_color ;
+		}
+		void set_number_of_cards( int input_number_of_cards)
+		{
+			number_of_card = input_number_of_cards ;
+		}
+		void set_number_of_played_cards( int input_number_of_played_cards)
+		{
+			number_of_played_card = input_number_of_played_cards ;
+		}
+		void set_pass( int input_pass )
+		{
+			pass = input_pass ;
+		}
+		void set_score( int input_score )
+		{
+			score = input_score ;
+		}
+		
+		string get_name()
+		{
+			return name ;
+		}
+		
+		int get_age()
+		{
+			return age ;
+		}
+		
+		string get_color()
+		{
+			return color ;
+		}
+		
+		int get_number_of_card()
+		{
+			return number_of_card ;
+		}
+		int get_number_of_played_card()
+		{
+			return number_of_played_card ;
+		}
+		int get_pass()
+		{
+			return pass;
+		}
+		int get_score()
+		{
+			return score;
+		}
+		
+		
+		void set_arry( card* my_card , int andis)
+		{
+			Arry_of_hand_card[andis] = my_card ;
+			//print_card();
+		}
+		
+		void print_card() 
+		{
+			for ( int i = 0 ; i < number_of_card ; i++)
+			{
+				//Arry_of_hand_card[i] -> print();
+				cout << Arry_of_hand_card[i] -> get_name() << " ";
+				//cout << Arry_of_hand_card[i] -> get_number_of_card() << " ";
+			}
+		}
+		void print_all_hand_cards( int andis )
+		{
+			cout << "cards of player : " << andis + 1 << endl ;
+			
+			for( int i = 0 ; i < number_of_card ; i++)
+			{
+				cout << Arry_of_hand_card[i] -> get_name() << "  ";
+			} 
+		}
+		
+		void play_this_card( string name , int andis_of_player )
+		{
+			bool find = 0 ;
+			
+			for ( int i = 0 ; i < number_of_card ; i++)
+			{
+				if ( name == Arry_of_hand_card[i] -> get_name() )
+				{
+		
+					Arry_of_hand_card[i] -> set_name("0"); 
+					//cout << "cout << number_of_card << endl ;"<<  number_of_card << endl ;
+					number_of_played_card++ ;
+					//cout << "cout << number_of_card << endl ;"<<  number_of_card << endl ;
+					find = 1;
+					break;
+				}
+				
+			}
+			if ( find == 0 )
+			{
+				cout << "you havent this card ! ";
+			}
+		}
+		
+	private :
+		card *Arry_of_hand_card[10];
+		
+		string name ;
+		int age ;
+		string color ;
+		int number_of_card = 10 ;
+		int number_of_played_card = 0 ;
+		int pass = 0 ;
+		int score = 0 ;
+};
+
+class control{
+	
+	public :
+		void start_game( int number_of_players)
+		{
+			toozi(number_of_players);
+			Players_Initializer( number_of_players );
+	        who_is_starter( number_of_players );
+	        one_door_game( number_of_players );
+		}
+	
+	void Players_Initializer( int number_of_players )
+	{	
+		string Name ;
+		int Age ;
+		
+		for ( int i = 0 ; i < number_of_players ; i++)
+		{
+			cout << "enter name and age of player "<< i+1 << " : " ;
+			cin >> Name ;
+			cin >> Age ;
+			playerArry[i].set_age( Age );
+			playerArry[i].set_name( Name );				
+		}			
+	}
+	
+	void card_initializer( int number_of_players)
+	{
+		yello_card yellow1( "1" , 10 , 1) ;
+		card_Arry[0] = &yellow1 ;
+		
+		yello_card yellow2( "2" , 8 , 2) ;
+		card_Arry[1] = &yellow2 ;
+		
+		yello_card yellow3( "3" , 8 , 3) ;
+		card_Arry[2] = &yellow3 ;
+		 
+		yello_card yellow4( "4" , 8 , 4) ;
+		card_Arry[3] = &yellow4 ; 
+		
+		yello_card yellow5( "5" , 8 , 5) ;
+		card_Arry[4] = &yellow5 ;
+		
+		yello_card yellow6( "6" , 8 , 6) ;
+		card_Arry[5] = &yellow6 ; 
+		
+		yello_card yellow10( "10" , 8 , 10) ;
+		card_Arry[6] = &yellow10 ;
+		
+		
+		Matarsak matarsak("matarsak" , 16 , 0);
+		card_Arry[7] = &matarsak ;
+		
+		
+		Tablzan tablzan("tablzan" , 6 , 3);
+		card_Arry[8] = &tablzan ;
+		
+		Shahdokht shahdokht("shahdokht" , 3 , 5);
+		card_Arry[9] = &shahdokht ;
+		
+		Bahar bahar("bahar" , 3 , 4);
+		card_Arry[10] = &bahar ;
+		
+		Zemestan zemestan("zemestan" , 3 , 2);
+		card_Arry[11] = &zemestan ;
+		
+		
+		start_game(number_of_players);
+		//ran time 
+			
+	}
+	
+	void toozi( int number_of_players)
+	{
+		int random ;
+		srand( time (0)) ;
+	    
+	    
+	    for ( int i = 0 ; i < number_of_players ; i++)
+	    {
+	    	int j = 0 ;
+	    	while (  j < playerArry[i].get_number_of_card() )
+	    	{
+	    		random = rand() % 12 ;
+	    		
+	    		if (card_Arry[random] -> get_number_of_card() != card_Arry[random] -> get_number_of_toozi_card() )
+	    		{
+	    			playerArry[i].set_arry( card_Arry[random] , j );
+	    			int num = card_Arry[random] -> get_number_of_toozi_card() + 1 ;
+	    			card_Arry[random] -> set_number_of_toozi_card( num );
+	    			j++ ;
+				}		
+			}	    	
+		}
+	
+	}
+	
+	int who_is_starter( int number_of_players )
+	{
+		int age_of_starter = playerArry[0].get_age() ;
+		
+		int starter = 1 ;
+		
+		for ( int i = 0 ; i < number_of_players ; i++ )
+		{
+			if ( playerArry[i].get_age()  < age_of_starter )
+			{
+				starter = i+1 ;
+			}
+		}
+		cout << endl << "starter : "<< starter ;
+		return starter;
+	}
+	
+	void one_door_game( /*int who_is_starter ,*/ int number_of_players )
+	{
+		for ( int i = 0 ; i < number_of_players ; i++ )
+		{
+			if ( playerArry[i].get_pass() == 0 )
+			{
+				play_card( i );
+			}
+			else 
+			{
+				cout << endl << "player " << i+1 << " you have passed " << endl ;
+			}
+		}	
+	}
+	
+	void play_card( int playerr )
+	{
+		
+		cout << endl << "player " << playerr + 1 << "your cards are : " << endl ;
+		print_hand_cards( playerr );
+		
+		string card_name ;
+		
+		if ( playerArry[playerr].get_pass() == 1 )
+		{
+			cout << endl << "you have passed" ;
+		}
+		
+		else {
+		
+			cout << endl << "choise your card to play : "<< endl ;
+			cin >> card_name ;
+		}
+		
+		if ( card_name == "pass")
+		{
+			cout << endl << "you passed " << endl ;
+			playerArry[playerr].set_pass(1);
+		}
+		
+		else {
+		
+			playerArry[playerr].play_this_card( card_name , playerr);
+		}			
+	}
+	
+	void print_hand_cards( int andis )
+	{
+		playerArry[andis].print_all_hand_cards(andis);
+	}
+	
+	
+	
+	private :
+		card *card_Arry[100];
+		player playerArry[6];
+		
+};
+int main()
+{
+	control object ;
+	
+	int number_of_players ;
+	cout << "enter number_of_players :";
+	cin >> number_of_players ;
+	
+	object.card_initializer(number_of_players);
+	
+	
+	//object.print_card_player( number_of_players);
+
+	
+	
+	
+	int a ;
+	cin >> a ;
+	
+	
+}
+
+
+
+
+
+
+
+
+
+
+
